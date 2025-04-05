cca6f5d273cdfcff02f1035dffc5cdf107947c89
ashwaniverma-github <ap8606574@gmail.com>
2025-04-05 16:10:32 +0530

    price-check

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -22,7 +22,7 @@ const Pricing: FC<CreditPricingProps> = ({ onBuy }) => {
     {
       title: "Starter Plan",
       credits: 200,
-      price: 5,
+      price: 1,
       paymentType: "one-time",
       features: [
         "200 credits",
